a16db71bfd712fd7ee433a901c4cb6836b9afe9c
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-14 20:43:09 +0300

    add navbar styling

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,30 @@
-import React from "react";
-import { Outlet, Link} from 'react-router-dom'
+import "../Layout.css"
+import {Outlet} from 'react-router-dom'
 
-function Layout(){
+export default function Layout(){
     return(
         <>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/blogs">Blogs</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-        </ul>
+        <nav className="navigation">
+            <a href="/" className="brand-name">MacroSoft</a>
+            <button className="hamburger">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
+                <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                />
+                </svg>
+            </button>
+            <div className="navigation-menu">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href='/blogs'>Blogs</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </div>
+        </nav>
 
-        <Outlet />
-
+        <Outlet/>
         </>
     )
 }
-
-export default Layout;
