fe20e0615baf9f8017c6646ff2c22ac9b0e567d2
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-14 21:06:35 +0300

    add css file link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./index.css"
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
